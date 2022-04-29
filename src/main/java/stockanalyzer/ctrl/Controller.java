a15b68d6c10ca7aae1bd85956a2828005e40f4f0
Dr. Schluk <lukas.schelepet@aon.at>
2022-04-29 23:26:15 +0200

    v1 Ich hab einfach mal das gemacht, was ich verstanden habe, ohne irgendwelche Fragen gestellt zu haben (war nicht vor Ort und deswegen ein bisschen am Verzweifeln) Bezüglich der Angabe ein paar (wichtige) Fragen: - Soll man Symbols oder gefilterte Werte dieser auswählen? - Was soll man mit dem searchString anfangen? Man kann explizite Werte doch nur mit get-Funktionen von Result.java bekommen (es sei denn es soll simpel über nen Switch vom searchString laufen, was ich mir nicht denken kann allein wegen der Anzahl der Werte) - Soll das Programm über eine längere Zeiteinheit laufen, periodisch Datensätze holen und aus x Datensätzen dann einen Höchstwert berechnen? (Alles reine Spekulation, da nix genaues in der Angabe steht) - Wenn der letzte Punkt zutrifft, wie soll dann closeConnection() den "GET-Loop" stoppen? Nach wievielen Datensätzen?
    Ohne Erklärung ist man bei so einer Übung auf jeden Fall komplett aufgeschmissen, also bitte ich um etwas Nachsicht mit den Personen, die nicht vor Ort waren (mich eingeschlossen)

--- src/main/java/stockanalyzer/ctrl/Controller.java
+++ src/main/java/stockanalyzer/ctrl/Controller.java
@@ -1,27 +1,45 @@
 package stockanalyzer.ctrl;
 
+import yahooApi.YahooFinance;
+import yahooApi.beans.QuoteResponse;
+import yahooApi.beans.YahooResponse;
+
+import java.util.*;
+
 public class Controller {
-		
+	YahooFinance yahooFinance = new YahooFinance();
+	List<String> tickers = Arrays.asList("AAPL", "AMZN", "TSLA", "GOOG", "OMV.VI",
+			"EBS.VI","DOC.VI","SBO.VI","RBI.VI","VIG.VI","TKA.VI","VOE.VI","FACC.VI","ANDR.VI","VER.VI",
+			"WIE.VI","CAI.VI","BG.VI","POST.VI","LNZ.VI","UQA.VI","SPI.VI","ATS.VI","IIA.VI");
 	public void process(String ticker) {
 		System.out.println("Start process");
 
-		//TODO implement Error handling 
 
-		//TODO implement methods for
-		//1) Daten laden
+		try {
+			getData(ticker);	//1) Daten laden
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
 		//2) Daten Analyse
-
+		//Was soll man analysieren??? Angabe unschlüssig
 	}
 	
 
-	public Object getData(String searchString) {
-
-		
+	public Object getData(String searchString /*Wozu? Ich kann mit dem doch keine get-Funktionen rausfiltern oder?*/) {
+		//String data = yahooFinance.requestData(tickers);
+		YahooResponse response = yahooFinance.getCurrentData(tickers);
+		QuoteResponse quotes = response.getQuoteResponse();
+		System.out.println("Aktienkurse: " + quotes.getResult().stream().count());
+		System.out.println("Aktienkurse mit \"A\" im Symbol: " + quotes.getResult().stream().filter(result -> result.getSymbol().contains("A")).count());
+		System.out.println("Symbol\t\tDay High\t\t50 Day Avrg\t\t");
+		quotes.getResult().forEach(quote -> System.out.println(quote.getSymbol() + ":\t\t" + quote.getRegularMarketDayHigh() + " \t\t" +  quote.getFiftyDayAverage()));
 		return null;
 	}
 
 
 	public void closeConnection() {
-		
+
 	}
 }
